321e6c6891816f8862c4c2ae5233991551e7f068
sumondeveloper <learnwithsumon@gmail.com>
2024-11-26 11:55:26 +0600

    add navbar components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,8 +26,21 @@ const Navbar = ({ className }: { className?: string }) => {
         </Link>
 
         <MenuItem setActive={setActive} active={active} item="Our Courses">
-          <HoveredLink href="/courses">All Courses</HoveredLink>
+          <div className="flex flex-col space-y-4">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses">Advenced Composition</HoveredLink>
+            <HoveredLink href="/courses">Songwriting</HoveredLink>
+            <HoveredLink href="/courses">Music Production</HoveredLink>
+          </div>
         </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact us"
+          ></MenuItem>
+        </Link>
       </Menu>
     </div>
   );
