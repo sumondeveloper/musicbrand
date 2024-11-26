07d90d5aacb66f0c7c9895cae89f7efdd7856611
sumondeveloper <learnwithsumon@gmail.com>
2024-11-26 12:01:24 +0600

    Add navbar component to the site

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-center ">Music Brand</h1>
+    </main>
+  );
 };
 export default Home;
