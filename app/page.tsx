b3adafcfab79a5af20dedba0a09140ebab5367c8
sumondeveloper <learnwithsumon@gmail.com>
2024-11-28 09:15:29 +0600

    sticky scroll section add to website

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import FeaturedSection from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 const Home = () => {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 };
