8dd23bd8220311e17c33c4d7636f35ad0e5f63a0
sumondeveloper <learnwithsumon@gmail.com>
2024-11-28 08:51:15 +0600

    add featured sectiion

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import FeaturedSection from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 const Home = () => {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 };
