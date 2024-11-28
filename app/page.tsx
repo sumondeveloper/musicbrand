f474a16bb17246a17e2a50e090220973370fcb6d
sumondeveloper <learnwithsumon@gmail.com>
2024-11-28 15:30:26 +0600

    add footer section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructor from "@/components/Instructor";
 import TestimonialCard from "@/components/TestimonialCard";
@@ -14,6 +15,7 @@ const Home = () => {
       <TestimonialCard />
       <Webminers />
       <Instructor />
+      <Footer />
     </main>
   );
 };
